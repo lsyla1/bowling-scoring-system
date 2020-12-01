d64597ec260f76c25d3ece18ffbae6518a0c8022
Andres Ortiz <andresortiz28@gmail.com>
2020-11-30 20:27:51 -0500

    Initial commit

---
+++ src/main/java/com/jobsity/bowling/BowlingApplication.java
@@ -0,0 +1,15 @@
+package com.jobsity.bowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Andres Ortiz
+ */
+@SpringBootApplication
+public class BowlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingApplication.class, args);
+	}
+}
