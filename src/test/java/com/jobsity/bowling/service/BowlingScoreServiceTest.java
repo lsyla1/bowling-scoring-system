09941305f67e4a15e642267f0ff8d9ad324390cd
Andres Ortiz <andresortiz28@gmail.com>
2020-12-07 08:34:23 -0500

    Added Unit and Integration tests

---
+++ src/test/java/com/jobsity/bowling/service/BowlingScoreServiceTest.java
@@ -0,0 +1,124 @@
+package com.jobsity.bowling.service;
+
+import com.jobsity.bowling.domain.*;
+import com.jobsity.bowling.exception.BowlingException;
+import com.jobsity.bowling.repository.FrameRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class BowlingScoreServiceTest {
+
+    @Mock
+    private FrameRepository frameRepository;
+
+    @InjectMocks
+    private BowlingScoreService scoreService;
+
+    private Game game;
+
+    private Player player;
+
+    private Score score;
+
+    @BeforeEach
+    void init() {
+        game = Game.builder().type(GameType.TEN_PIN_BOWLING).build();
+        player = new Player();
+
+        score = new Score();
+        score.setId(new ScoreKey(game.getId(), player.getId()));
+        score.setStatus(ScoreStatus.COMPLETED);
+
+        addFrameWithRolls(1, new int[]{10});
+        addFrameWithRolls(2, new int[]{7, 3});
+        addFrameWithRolls(3, new int[]{9, 0});
+        addFrameWithRolls(4, new int[]{10});
+        addFrameWithRolls(5, new int[]{0, 8});
+        addFrameWithRolls(6, new int[]{8, 2});
+        addFrameWithRolls(7, new int[]{0, 6});
+        addFrameWithRolls(8, new int[]{10});
+        addFrameWithRolls(9, new int[]{10});
+        addFrameWithRolls(10, new int[]{10, 8, 1});
+    }
+
+    void addFrameWithRolls(int frameNumber, int[] pins) {
+        Frame frame = new Frame();
+        frame.setNumber(frameNumber);
+
+        IntStream.range(0, pins.length).forEach(i -> {
+            Roll roll = new Roll();
+            roll.setPins(pins[i]);
+            frame.addRoll(roll);
+        });
+
+        score.addFrame(frame);
+    }
+
+    @Test
+    void getPinfallsPerFrame() throws BowlingException {
+        List<String> list = new ArrayList<>();
+        list.add("\tX");
+        list.add("7\t/");
+        list.add("9\t0");
+        list.add("\tX");
+        list.add("0\t8");
+        list.add("8\t/");
+        list.add("0\t6");
+        list.add("\tX");
+        list.add("\tX");
+        list.add("X\t8\t1");
+
+        List<String> result = scoreService.getPinfallsPerFrame(score);
+
+        assertEquals(list.size(), result.size());
+        boolean diff = false;
+        for (int i = 0; i < list.size(); i++) {
+            if(!list.get(i).equals(result.get(i))) {
+                diff = true;
+                break;
+            }
+        }
+        assertFalse(diff);
+    }
+
+    void getScoresPerFrame() throws BowlingException {
+        when(frameRepository.findFrameByNumberAndScore(anyInt(), any(Score.class)))
+                .thenAnswer(i -> score.getFrames().get(i.getArgument(0, Integer.class) - 1));
+
+        List<String> list = new ArrayList<>();
+        list.add("20");
+        list.add("39");
+        list.add("48");
+        list.add("66");
+        list.add("74");
+        list.add("84");
+        list.add("90");
+        list.add("120");
+        list.add("148");
+        list.add("167");
+
+        List<String> result = scoreService.getScoresPerFrame(score);
+
+        assertEquals(list.size(), result.size());
+        boolean diff = false;
+        for (int i = 0; i < list.size(); i++) {
+            if(!list.get(i).equals(result.get(i))) {
+                diff = true;
+                break;
+            }
+        }
+        assertFalse(diff);
+    }
+}
---
+++ src/test/java/com/jobsity/bowling/util/BowlingUtilTest.java
@@ -0,0 +1,101 @@
+package com.jobsity.bowling.util;
+
+import com.jobsity.bowling.domain.Frame;
+import com.jobsity.bowling.domain.Roll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingUtilTest {
+
+    private Frame frame;
+
+    @BeforeEach
+    void init() {
+        frame = new Frame();
+    }
+
+    void addRollToFrame(int pins) {
+        Roll roll = new Roll();
+        roll.setPins(pins);
+        frame.addRoll(roll);
+    }
+
+    @Test
+    void isStrike() {
+        addRollToFrame(10);
+        assertTrue(BowlingUtil.isStrike(frame));
+    }
+
+    @Test
+    void isSpare() {
+        addRollToFrame(2);
+        addRollToFrame(8);
+        assertTrue(BowlingUtil.isSpare(frame));
+    }
+
+    @Test
+    void isFrameCompletedAsNormal() {
+        frame.setNumber(1);
+        addRollToFrame(4);
+        addRollToFrame(4);
+        assertTrue(BowlingUtil.isFrameCompleted(frame));
+    }
+
+    @Test
+    void isFrameCompletedAsNormalAsStrike() {
+        frame.setNumber(1);
+        addRollToFrame(10);
+        assertTrue(BowlingUtil.isFrameCompleted(frame));
+    }
+
+    @Test
+    void isFrameCompletedAsNormalAsSpare() {
+        frame.setNumber(1);
+        addRollToFrame(4);
+        addRollToFrame(6);
+        assertTrue(BowlingUtil.isFrameCompleted(frame));
+    }
+
+    @Test
+    void isFrameCompletedAsTenth() {
+        frame.setNumber(10);
+        addRollToFrame(4);
+        addRollToFrame(4);
+        assertTrue(BowlingUtil.isFrameCompleted(frame));
+    }
+
+    @Test
+    void isFrameCompletedAsTenthAsStrike() {
+        frame.setNumber(10);
+        addRollToFrame(10);
+        addRollToFrame(5);
+        addRollToFrame(1);
+        assertTrue(BowlingUtil.isFrameCompleted(frame));
+    }
+
+    @Test
+    void isFrameCompletedAsTenthAsSpare() {
+        frame.setNumber(10);
+        addRollToFrame(4);
+        addRollToFrame(6);
+        addRollToFrame(1);
+        assertTrue(BowlingUtil.isFrameCompleted(frame));
+    }
+
+    @Test
+    void getPureScore() {
+        addRollToFrame(4);
+        addRollToFrame(5);
+        assertEquals(BowlingUtil.getPureScore(frame), 9);
+    }
+
+    @Test
+    void getPinsOfTwoRolls() {
+        addRollToFrame(4);
+        addRollToFrame(3);
+        addRollToFrame(1);
+        assertEquals(BowlingUtil.getPinsOfTwoRolls(frame), 7);
+    }
+}
