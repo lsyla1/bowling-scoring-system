701595eb5d21bcb5b5a66905971a035800cabc46
Andres Ortiz <andresortiz28@gmail.com>
2020-12-14 08:02:10 -0500

    Added comments to interfaces

--- src/main/java/com/jobsity/bowling/service/BowlingSystemService.java
+++ src/main/java/com/jobsity/bowling/service/BowlingSystemService.java
@@ -2,34 +2,61 @@ package com.jobsity.bowling.service;
 
 import com.jobsity.bowling.domain.*;
 import com.jobsity.bowling.exception.*;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * Class that process a Bowling game
+ * @author Andres Ortiz
+ */
 @Service
 public class BowlingSystemService implements SystemService {
 
+    /**
+     * Game Service
+     */
     @Autowired
     private GameService gameService;
 
+    /**
+     * Score Service
+     */
     @Autowired
     private ScoreService<String> scoreService;
 
+    /**
+     * Game instance for each game file to process
+     */
     private Game game;
 
+    /**
+     * Players queue to check the correct order
+     */
     private Queue<Player> players;
 
+    /**
+     * Previous player reference
+     */
     private Player previousPlayer;
 
+    /**
+     * Flag to identify when the players order is established
+     */
     private boolean orderFound;
 
+    /**
+     * Process a bowling game given a game file path
+     * @param filePath Game file path
+     * @return The dashboard result of the bowling game
+     * @throws BowlingException Occurs when a bowling rule is broken
+     * @throws IOException Occurs when the file path is invalid or does not exit.
+     */
     @Override
     public String processGame(String filePath) throws BowlingException, IOException {
         List<String> lines = Files.lines(Paths.get(filePath)).collect(Collectors.toList());
@@ -45,12 +72,23 @@ public class BowlingSystemService implements SystemService {
         return scoreService.getResults(game);
     }
 
+    /**
+     * Add a Player
+     * @param newPlayer Non-existing player
+     * @return The new player added in the database
+     */
     private Player addPlayer(Player newPlayer) {
         Player player = gameService.addPlayer(game, newPlayer);
         players.add(player);
         return player;
     }
 
+    /**
+     * Add Point to a Player
+     * @param player Existing or new player
+     * @param points Number of pins or 'F' for a foul
+     * @throws BowlingException Occurs when a bowling rule is broken
+     */
     private void addPoints(Player player, String points) throws BowlingException {
         if (players.isEmpty()) {
             Player currentPlayer = addPlayer(player);
@@ -85,6 +123,11 @@ public class BowlingSystemService implements SystemService {
         }
     }
 
+    /**
+     * Validate the input format
+     * @param line Row of input file
+     * @throws IncorrectFormatException Occurs due to an error in the input format
+     */
     private void validateFormat(String line) throws IncorrectFormatException {
         String[] data = line.split(" ");
         if (data.length != 2) {
@@ -98,6 +141,11 @@ public class BowlingSystemService implements SystemService {
         }
     }
 
+    /**
+     * Validate the order or turn of the Player
+     * @param player Existing or new player
+     * @throws BowlingException Occurs when a bowling rule is broken
+     */
     private void validateOrder(Player player) throws BowlingException {
         if (!player.getName().equals(previousPlayer.getName()) && !gameService.isFrameFinished(game, previousPlayer)) {
             throw new PlayerOrderException("It is still the turn of the player " + previousPlayer.getName());
